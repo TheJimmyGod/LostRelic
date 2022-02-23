07c238fefcd5827730c65c4d3ff8458f9fd62821
wallacegao <13730852881@163.com>
2022-02-23 09:58:02 +0900

    add finish puzzle 1 , where player need enter tigger to open the door

---
+++ GameJam/Assets/Scripts/Puzzle/Tigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tigger : MonoBehaviour
+{
+    public bool isActive = false;
+    [SerializeField]
+    private Material mMaterial;
+    [SerializeField]
+    public Color mColorsActive;
+    [SerializeField]
+    public Color mColorsNoneActive;
+
+    private void Update()
+    {
+        if (isActive)
+        {
+            mMaterial.color = mColorsActive;
+        }
+        else
+        {
+            mMaterial.color = mColorsNoneActive;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isActive = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isActive = false;
+    }
+}
